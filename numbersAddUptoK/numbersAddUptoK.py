9d53e8f67e21a965a416c51ce1631e803144256f
AkileshRaoG <akinih07@gmail.com>
2019-06-03 14:57:28 +0530

    Rearranged folder structure

--- numbersAddUptoK/numbersAddUptoK.py
+++ numbersAddUptoK/numbersAddUptoK.py
@@ -1,18 +1,6 @@
 #Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 #For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
-'''
-1) Sort the array in non-decreasing order.
-2) Initialize two index variables to find the candidate 
-   elements in the sorted array.
-       (a) Initialize first to the leftmost index: l = 0
-       (b) Initialize second  the rightmost index:  r = ar_size-1
-3) Loop while l < r.
-       (a) If (A[l] + A[r] == sum)  then return 1
-       (b) Else if( A[l] + A[r] <  sum )  then l++
-       (c) Else r--    
-4) No candidates in whole array - return 0
-'''
 
 def ProcessList(inputList , valueToCheck):
 	lenOfList = len(inputList)
